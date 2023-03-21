89c6c699afd736e47b3e0c0b01814a366fd30db0
Rod Stephine D. Sarsaba <rodsarsaba26@gmail.com>
2023-03-22 01:02:13 +0800

    (ROD) modified fixed Error

--- src/shared/components/RootLayouts.js
+++ src/shared/components/RootLayouts.js
@@ -1,11 +1,11 @@
-import Header from './Component/Header'
-import Footer from './Component/Footer'
+import Header from '../../Component/Header'
+import Footer from '../../Component/Footer'
 
-const RootLayouts = ( { children } ) => {
+const RootLayouts = ( ) => {
     return (
       <div className="root-main">
         <Header></Header>
-          {children}
+          
         <Footer></Footer>
       </div>
     );

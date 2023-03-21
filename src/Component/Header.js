75b086ec65fb84292040351ce2c5ae3dd94f3cec
Rod Stephine D. Sarsaba <rodsarsaba26@gmail.com>
2023-03-22 01:01:14 +0800

    (Rod) Modified Header add an top header above of nav

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,18 +1,73 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Button,Container,Form,Nav,Navbar,Modal,Row,Col,Image} from "react-bootstrap";
 import logo from '../assets/logo-1.jpg'
-import Modal from 'react-bootstrap/Modal';
+import phoneLogo from "../assets/icons8-phone-24.png"
 import React, { useState } from 'react';
 
 function Header () {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  return (
+
+  return ( 
+    <>
+    {/* <header>
+  <div className="container-fluid bg-dark text-light">
+    <div className="header-cont d-flex gap-3 px-4 py-3 align-content-center">
+      <div className="row">
+        <p className="col-md-2 p-0 m-0 text-nowrap">CALL US:</p>
+        <div className="col-md-5 d-flex gap-1">
+          <img width="20px" height="20px" src={phoneLogo} alt="phone logo" />
+          <p className="text-nowrap p-0 m-0">(02)8-921-0000</p>
+        </div>
+        <div className="col-md-5 d-flex gap-1">
+          <img width="20px" height="20px" src={phoneLogo} alt="phone logo" />
+          <p className="text-nowrap p-0 m-0">(02)8-921-0000</p>
+        </div>
+      </div>
+      <div className="d-flex gap-3 align-content-center ms-auto">
+        <a href="/">
+          <img width="25px" src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_facebook-256.png" alt="facebook logo" />
+        </a>
+        <a href="/">
+          <img width="25px" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter3_colored_svg-512.png" alt="Twitter logo" />
+        </a>
+        <a href="/">
+          <img width="25px" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-128.png" alt="Instagram logo" />
+        </a>
+      </div>
+    </div>
+  </div>
+  </header> */} 
+
+<header>
+  <Container fluid className="bg-dark text-light">
+    <div className="header-cont d-flex gap-3 px-4 py-3 align-content-center">
+      <Row className="row">
+        <Col md={2} className="p-0 m-0 text-nowrap">CALL US:</Col>
+        <Col md={5} className="d-flex gap-1">
+          <Image width="20px" height="20px" src={phoneLogo} alt="phone logo" />
+          <p className="text-nowrap p-0 m-0">(02)8-921-0000</p>
+        </Col>
+        <Col md={5} className="d-flex gap-1">
+          <Image width="20px" height="20px" src={phoneLogo} alt="phone logo" />
+          <p className="text-nowrap p-0 m-0">(02)8-921-0000</p>
+        </Col>
+      </Row>
+      <div className="d-flex gap-3 align-content-center ms-auto">
+        <Nav.Link href="/">
+          <Image width="25px" src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_facebook-256.png" alt="facebook logo" />
+        </Nav.Link>
+        <Nav.Link href="/">
+          <Image width="25px" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter3_colored_svg-512.png" alt="Twitter logo" />
+        </Nav.Link>
+        <Nav.Link href="/">
+          <Image width="25px" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-128.png" alt="Instagram logo" />
+        </Nav.Link>
+      </div>
+    </div>
+  </Container>
+  </header>
+
     <Navbar expand="lg" className="navbar">
       <Container fluid className='mx-4'>
         <Navbar.Brand href="/" className="navbar-brand me-5 px-2">
@@ -39,27 +94,27 @@ function Header () {
             />
             <Button variant="outline-light">Search</Button>
           </Form>
-          <div class="navbar-nav m-2">
+          <div className="navbar-nav m-2">
                 <Nav.Link bg-none href="#modalScrollableCenter" data-bs-toggle="modal" onClick={handleShow} className="wish-zoom ms-auto nav-item nav-link">
-                    <img width="30px" height="30px" src="https://cdn3.iconfinder.com/data/icons/jolly-icons-free/64/cart_64.png" alt=""/><span class="badge bg-danger">4</span></Nav.Link>
-                <a class="ms-auto wish-zoom nav-item nav-link" href="#modalSignin"  data-bs-toggle="modal">
+                    <img width="30px" height="30px" src="https://cdn3.iconfinder.com/data/icons/jolly-icons-free/64/cart_64.png" alt=""/><span className="badge bg-danger">1</span></Nav.Link>
+                <a className="ms-auto wish-zoom nav-item nav-link" href="#modalSignin"  data-bs-toggle="modal">
                     <img width="30px" height="30px" src="https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_user-64.png" alt="Log in"/></a>
                 </div>
         </Navbar.Collapse>
       </Container>
     
-    <Modal id="modalScrollableCenter" className='modal-lg modal-dialog-centered modal-dialog-scrollable shadow-lg' show={show} onHide={handleClose}>
+    <Modal id="modalScrollableCenter" className='modal-lg modal-dialog-centered modal-dialog-scrollable shadow-lg modal-dialog' centered show={show} onHide={handleClose}>
       
         <Modal.Header closeButton>
           <Modal.Title className='mx-3'>
-            <h3 class="modal-title">Your Cart</h3>
+            <h3 className="modal-title">Your Cart</h3>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body className='mx-3'>
-          <div class="d-md-flex gap-2">
-            <img width="50px" height="50px" src="./projectassets/acc1.jpg" alt=""/><p class="mb-0 fw-bolder ms-3">LOTUS BRUSHLESS 18V X-Line Cordless Impact Drill w/2pcs Battery Charger & Hard Case LTHD18VLI-2BLX</p>
-                        <button type="button" class="py-0 text-nowrap btn-sm btn btn-danger text-light">Cancel</button>
-                        <button type="button" data-bs-target="" class="py-0 text-nowrap btn-sm btn btn-primary text-light" data-href="checkout.html">Check-out</button></div>
+          <div className="d-md-flex gap-2">
+            <img width="50px" height="50px" src="./projectassets/acc1.jpg" alt=""/><p className="mb-0 fw-bolder ms-3">LOTUS BRUSHLESS 18V X-Line Cordless Impact Drill w/2pcs Battery Charger & Hard Case LTHD18VLI-2BLX</p>
+                        <button type="button" className="py-0 text-nowrap btn-sm btn btn-danger text-light">Cancel</button>
+                        <button type="button" data-bs-target="" className="py-0 text-nowrap btn-sm btn btn-primary text-light" data-href="checkout.html">Check-out</button></div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={handleClose}>
@@ -71,6 +126,7 @@ function Header () {
         </Modal.Footer>
       </Modal>
     </Navbar>
+    </>
   );
 }
 
